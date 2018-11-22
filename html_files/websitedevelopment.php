caf81cdd6597b7e5812ea251e9de19f5649e6291
lovesharma1995 <45064534+lovesharma1995@users.noreply.github.com>
2018-11-22 16:22:50 +0530

    html files
    html files which are changed

--- html_files/websitedevelopment.php
+++ html_files/websitedevelopment.php
@@ -4,40 +4,39 @@
 <head>
     <title>Fiori Technologies</title>
     <link href="../css_files/common.css" type="text/css" rel="stylesheet" />
-    <link href="../css_files/websitedevelopment.css" type="text/css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
 </head>
 
 <body>
-    <div class="image">
+    <div class="image1">
         <div>
             <img src="../images/Fiori.png" height="90px" alt="FioriTech logo">
             <?php include 'common files/menu.php' ?>
         </div>
-        <div class="webdev">
-            <p class="first"><b>Website Development</b></p>
+        <div class="webdesign">
+            <p><b>Website Development</b></p>
         </div>
     </div>
     <div class="firstblock">
-        <p class="second">CMS (Content Management System) based website are for the ease of customer. If you want to update the things and features on your website on daily basis then CMS should be your choice. We can call it a Semi Dynamic Website and the site would appear as a normal website to the end user. You will get a control panel where you can login with a admin Id and password and enjoy the following advantages:</p>
+        <p>CMS (Content Management System) based website are for the ease of customer. If you want to update the things and features on your website on daily basis then CMS should be your choice. We can call it a Semi Dynamic Website and the site would appear as a normal website to the end user. You will get a control panel where you can login with a admin Id and password and enjoy the following advantages:</p>
     </div>
-    <div class="mainblock">
-        <div class="secondblock">
+        <div class="secondblock1">
             <ul>
-                <li class="csecondblock">Unlimited Pages.</li>
-                <li class="csecondblock">You can change the back ground.</li>
-                <li class="csecondblock">Can change the colour and theme on your website.</li>
-                <li class="csecondblock">Can edit any Page, Video, Audio & Image.</li>
-                <li class="csecondblock">Create and add new pages yourself instantly.</li>
+                <li class="csecondblock1">Unlimited Pages.</li>
+                <li class="csecondblock1">You can change the back ground.</li>
+                <li class="csecondblock1">Can change the colour and theme on your website.</li>
+                <li class="csecondblock1">Can edit any Page, Video, Audio & Image.</li>
+                <li class="csecondblock1">Create and add new pages yourself instantly.</li>
             </ul>
         </div>
         <div>
-            <img src="../images/web_designing_companies.png" alt="image" class="image2" />
+            <img src="../images/web_designing_companies.png" alt="image" class="image3" />
         </div>
+		<div class="clear"></div>
         <div>
-            <h2 class="bsecondblock"><b>Dynamic Website</b></h2>
-            <p class="third">If you have some idea in your mind or want to make a website which involves user interaction to user control the website need to be dynamic. Dynamic website generally involves complex coding, scripts and management of database. All these things brings together to deliver a site which can be used by the end users themselves. That how the masterpieces like: Naukri.com, Facebook.com, Shaadi.com, Flipkart.com are built. The idea of dynamic site are basically to give the access to the user (end user) as to create and maintain their profiles so that they can keep the information and change their details when required. At the same time it provide the ease of control for the owner of site with an admin panel through which one can control (edit/ change/ delete) any information on the website.</p>
-            <p class="third">We have following two types of packages to meet your dynamic concepts (based on different types of websites prevailing in the industry:</p>
+            <h2 class="bsecondblock1"><b>Dynamic Website</b></h2>
+            <p class="fourth">If you have some idea in your mind or want to make a website which involves user interaction to user control the website need to be dynamic. Dynamic website generally involves complex coding, scripts and management of database. All these things brings together to deliver a site which can be used by the end users themselves. That how the masterpieces like: Naukri.com, Facebook.com, Shaadi.com, Flipkart.com are built. The idea of dynamic site are basically to give the access to the user (end user) as to create and maintain their profiles so that they can keep the information and change their details when required. At the same time it provide the ease of control for the owner of site with an admin panel through which one can control (edit/ change/ delete) any information on the website.</p>
+            <p class="fourth">We have following two types of packages to meet your dynamic concepts (based on different types of websites prevailing in the industry:</p>
         </div>
 
         <div>
