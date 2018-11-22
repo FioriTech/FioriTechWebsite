fa3777d9941dd1c2905aa2ddbd46cd6a03f25bf6
vijay-garg-tpc <vijay.garg@tpcus.com>
2018-11-22 11:41:29 -0500

    css cleanup

--- html_files/career.php
+++ html_files/career.php
@@ -14,57 +14,58 @@
             <img src="../images/Fiori.png" height="90px" alt="FioriTech Logo">
             <?php include 'common files/menu.php' ?>
         </div>
-        <div style="clear:both;"></div>
-        <div class="firstblock">
-            <p class="second">Fiori Technologies is the youngest and fastest growing technology enabler in Website Development, Digital Marketing and Cyber Security. We want to harness the talent of all our employees to be able to make a difference and create an exciting future for all. Our employees tell us they value the helpful, collaborative and supportive culture that exists within the organization.</p>
-        </div>
-        <h1 class="openings"> Current Openings</h1>
+    </div>
+    <div class="intro">
+        <p>Fiori Technologies is the youngest and fastest growing technology enabler in Website Development, Digital Marketing and Cyber Security. We want to harness the talent of all our employees to be able to make a difference and create an exciting future for all. Our employees tell us they value the helpful, collaborative and supportive culture that exists within the organization.</p>
+    </div>
+    <h1 class="openings"> Current Openings</h1>
 
-        <div class="mainblock">
-            <div class="amainblock">
-                <h1 class="web"> Digital Marketing Executive</h1>
-                <pre class="aweb">Location: Delhi
+    <div class="mainblock">
+        <div class="amainblock">
+            <h1 class="web"> Digital Marketing Executive</h1>
+            <pre class="aweb">Location: Delhi
 Job Code:-00102
 Years of experience: 0-3 Years
 No. of Openings - 1</pre>
-                <pre class="first">Requirements:
+            <pre>Requirements:
 Good skills in Email Marketing, Content Marketing, Web Analytics, SEO & Social Media Marketing.
 Excellent verbal and written English communication. 
 Eagerness to learn and grow in the digital marketing industry.
 Analytical mindset and critical thinking.
 </pre>
-                <a href="job_application_form.php" class="links">APPLY</a>
-                <br />
-                <br />
-            </div>
-            <hr />
-            <div class="bmainblock">
-                <h1 class="web"> Senior Web Developer</h1>
-                <pre class="aweb">Location: Delhi
+            <a href="job_application_form.php" class="links">APPLY</a>
+            <br />
+            <br />
+        </div>
+        <hr />
+        <div class="bmainblock">
+            <h1 class="web"> Senior Web Developer</h1>
+            <pre class="aweb">Location: Delhi
 Job Code:-00102
 Years of experience: 2-5 Years
 No. of Openings - 3</pre>
-                <pre class="first">Requirements:
+            <pre>Requirements:
 Good skills in Core PHP, wordpress, Magento, Shopify, MYSQL, XML, HTML, DHTML, Java Script, JQuery. 
 Google & Third Party API Integration
 Developed PHP Applications with various IDE tools like Eclipse.</pre>
-                <a href="job_application_form.php" class="links">APPLY</a>
-            </div>
-            <hr />
-            <div class="cmainblock">
-                <h1 class="web"> Web Developer</h1>
-                <pre class="aweb">Location: Delhi
+            <a href="job_application_form.php" class="links">APPLY</a>
+        </div>
+        <hr />
+        <div class="cmainblock">
+            <h1 class="web"> Web Developer</h1>
+            <pre class="aweb">Location: Delhi
 Job Code:-00101
 Years of experience: 0-1 Year
 No. of Openings - 2</pre>
-                <pre class="first">Requirements:
+            <pre>Requirements:
 Need experience in HTMLCSS, JavaScript, jQuery, Ajax
 Good with PHP, MySQL, Bootstrap
 Very good communication skills </pre>
-                <a href="job_application_form.php" class="links">APPLY</a>
-            </div>
+            <a href="job_application_form.php" class="links">APPLY</a>
         </div>
-        
+    </div>
+
     <?php include 'common files/footer.php' ?>
 </body>
+
 </html>
