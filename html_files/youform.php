1f353446b4b5ab8e6eb25f48b8e5d6f85170010d
vijay-garg-tpc <vijay.garg@tpcus.com>
2018-11-22 01:25:41 -0500

    cleanup

--- html_files/youform.php
+++ html_files/youform.php
@@ -29,15 +29,12 @@
         <p>Forms can be as much of a problem for end users as for the people managing them. For this reason we’re heavily focused on creating a great user experience. We do this by following a rulebook created by on the greatest usability legends of our time, Dr. Jacob Nielsen.By adopting his teachings we’ve created multiple features which will make your users particulary happy which will ultimately means great data and form follow through for the business managing this.One simple example of how we do this is allowing the user to fill out forms in three different ways, HTML, PDF as an attachment or Hosted PDF which are accessible and usable via Desktop, Mobile and Tablet.</p>
         <img src="../images/kralanx.png" alt="image">
     </div>
-    <div style="clear:both;"></div>
-
+    <br>
     <h1 class="question">Redefining the Way You Use Forms</h1>
     <div class="thirdpara">
         <p>YouForm FMS can be used to optimize pracatically every industry so long as dealing with forms are a daily part of your business. YouForm is packed with a plehtora of basic and advanced feautres to improve your day to day business. One great example of one of our feature list is in our master form structure. This allows you make a single change to your master form which instantly affects all forms using that particular master and their subsequent layers. This just one of the reasons why YouForm will turn what used to be an administrative nightmare into a smooth transistion allowing you time to focus on what really matters.</p>
     </div>
-
    
     <?php include 'common files/footer.php' ?>
 </body>
-
 </html>
