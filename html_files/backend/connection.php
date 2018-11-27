5edafc32354c119cbfea6c8b9b725ab68f75b28e
vijay-garg-tpc <vijay.garg@tpcus.com>
2018-11-27 17:42:13 -0500

    db connection

--- html_files/backend/connection.php
+++ html_files/backend/connection.php
@@ -1,6 +1,6 @@
 <?php
-$db="fioritech";
-$con=mysqli_connect("localhost","root","",$db);
+$db="fiori8b_webdb";
+$con=mysqli_connect("localhost","fiori8b_admin","Welcome@1",$db);
 if(!$con){
 	die("connection failed:".mysqli_connect_error());
 }
