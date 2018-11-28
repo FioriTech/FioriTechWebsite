e45bd947e36ef04fc633c943fdb5b383d83fc279
lovesharma1995 <45064534+lovesharma1995@users.noreply.github.com>
2018-11-28 17:28:42 +0530

    added canada address

--- html_files/contactus.php
+++ html_files/contactus.php
@@ -27,18 +27,29 @@
     </div>
     <div class="contactdetail">
         <h1>Contact Details</h1>
-        <div class="detail">
-            <img src="../images/kisspng-computer-icons-telephone-call-symbol-phone-vector-5ad050946903f3.3287647915236015564302.png" alt="icon">
-            <p>+91-7210603373</p>
+		<div class="canada">
+		<h3>Canada Office:</h3>
+		 <div class="detail1">
+            <img src="../images/vector-free-download-png-3.png" alt="icon">
+            <p class="address1">2233 Argentia Road, Suites 302 & 302A, Mississauga, Ontario, L5N 2X7, Canada</p>
         </div>
-        <div class="detail">
-            <img src="../images/kisspng-email-computer-icons-maxcuttm-inc-email-vector-5adb312ae6e962.6943741815243144109458.png" alt="icon">
-            <p>info@FioriTech.com</p>
-        </div>
+		</div>
+		
+		<div class="india">
+		<h3>India Office:</h3>
         <div class="detail">
             <img src="../images/vector-free-download-png-3.png" alt="icon">
             <p class="address">356, Aggarwal Metro Heights, Netaji Subash Place, Pitampura, Delhi-110034</p>
         </div>
+        <div class="detail">
+            <img src="../images/kisspng-computer-icons-telephone-call-symbol-phone-vector-5ad050946903f3.3287647915236015564302.png" alt="icon">
+            <p>+91-7210603373</p>
+        </div>
+		<div class="detail">
+            <img src="../images/kisspng-email-computer-icons-maxcuttm-inc-email-vector-5adb312ae6e962.6943741815243144109458.png" alt="icon">
+            <p>info@FioriTech.com</p>
+        </div>
+		</div>
     </div>
    
     <?php include 'common files/footer.php' ?>
