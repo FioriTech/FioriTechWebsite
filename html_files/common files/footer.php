59df42890acad10d59f70ad5fb390345c0ac87c5
lovesharma1995 <45064534+lovesharma1995@users.noreply.github.com>
2018-11-30 20:21:27 +0530

    google analytics code added

--- html_files/common files/footer.php
+++ html_files/common files/footer.php
@@ -11,3 +11,13 @@
 <a href="https://www.linkedin.com/company/fioritechnologies/" class="widgets" target="_blank"><img src="<?php echo $root_url.$sub_folder ?>linkedin.png" alt="linkedin logo" height="40px"></a>
 <a href="https://twitter.com/FioriTech" target="_blank" class="widgets"><img src="<?php echo $root_url.$sub_folder ?>twitter.png" alt="twitter logo" height="40px"></a>
 <a href="https://www.facebook.com/FioriTech/" class="widgets" target="_blank"><img src="<?php echo $root_url.$sub_folder ?>facebook.png" alt="facebook logo" height="40px"></a>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-130199622-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-130199622-1');
+</script>
