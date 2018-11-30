01cd0639b9559f33128588b7622cf2363c616325
lovesharma1995 <45064534+lovesharma1995@users.noreply.github.com>
2018-11-30 18:39:33 +0530

    added google maps

--- html_files/contactus.php
+++ html_files/contactus.php
@@ -28,29 +28,30 @@
     </div>
     <div class="contactdetail">
         <h1>Contact Details</h1>
-		<div class="canada">
-		<h3>Canada Office:</h3>
-		 <div class="detail">
-            <img src="../images/vector-free-download-png-3.png" alt="icon">
-            <p class="address">2233 Argentia Road, Suites 302 & 302A, Mississauga, Ontario, L5N 2X7, Canada</p>
-        </div>
-		</div>
-		
-		<div class="canada">
-		<h3>India Office:</h3>
-        <div class="detail">
-            <img src="../images/vector-free-download-png-3.png" alt="icon">
-            <p class="address">356, Aggarwal Metro Heights, Netaji Subash Place, Pitampura, Delhi-110034</p>
-        </div>
-        <div class="detail">
-            <img src="../images/kisspng-computer-icons-telephone.png" alt="icon">
-            <p>+91-7210603373</p>
-        </div>
-		<div class="detail">
+		<div class="detailemail">
             <img src="../images/kisspng-email.png" alt="icon">
             <p>info@FioriTech.com</p>
         </div>
+		
+		
+		 <div class="detail">
+		 <h3>Canada Office:</h3>
+            <img src="../images/vector-free-download-png-3.png" alt="icon">
+            <p class="address">2233 Argentia Road, Suites 302 & 302A, Mississauga, Ontario, L5N 2X7, Canada.</p>
+			<br>
+			<div class="mapouter"><div class="gmap_canvas"><iframe width="404" height="163" id="gmap_canvas" src="https://maps.google.com/maps?q=2233%20Argentia%20Road%2C%20Suites%20302%20%26%20302A%2C%20Mississauga%2C%20Ontario%2C%20L5N%202X7%2C%20Canada&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.pureblack.de">pure black</a></div>
+			</div>
 		</div>
+		
+		
+        <div class="detail">
+		<h3>India Office:</h3>
+            <img src="../images/vector-free-download-png-3.png" alt="icon">
+            <p class="address">356, Aggarwal Metro Heights, Netaji Subash Place, Pitampura, Delhi-110034.<br>
+			Contact: +91-7210603373</p>
+			<div class="mapouter"><div class="gmap_canvas"><iframe width="404" height="163" id="gmap_canvas" src="https://maps.google.com/maps?q=356%2C%20Aggarwal%20Metro%20Heights%2C%20Netaji%20Subash%20Place%2C%20Pitampura%2C%20Delhi-110034&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.pureblack.de">pure black</a></div>
+			</div>
+        </div>
     </div>
    
     <?php include 'common files/footer.php' ?>
